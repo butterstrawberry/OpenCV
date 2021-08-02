57b9b3dc73dd3cda6584d1f637e924179e6e6941
unexistentman <chan3358@naver.com>
2021-08-02 23:50:07 +0900

    add

---
+++ 04/0405.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread('./04/data/lena.jpg')
+
+img[100:400, 200:300, 0] = 255
+img[100:400, 300:400, 1] = 255
+img[100:400, 400:500, 2] = 255 
+
+cv2.imshow('img', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
