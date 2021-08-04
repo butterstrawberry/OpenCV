3555071be78de99fbd6b62df49af9ffa8fc2586a
unexistentman <chan3358@naver.com>
2021-08-04 11:10:51 +0900

    add

---
+++ 04/0407.py
@@ -0,0 +1,12 @@
+import cv2
+
+src = cv2.imread('./04/data/lena.jpg', cv2.IMREAD_GRAYSCALE)
+roi = cv2.selectROI(src)
+print('roi =', roi)
+
+img = src[roi[1]:roi[1] + roi[3],
+          roi[0]:roi[0] + roi[2]]
+    
+cv2.imshow('img', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
