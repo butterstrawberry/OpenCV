0a35a801b5d921f08ac793a334a6ed6d1abcb84f
unexistentman <chan3358@naver.com>
2021-08-31 18:21:24 +0900

    add

---
+++ py/05/0505.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+src = cv2.imread('./py/05/data/lena.jpg')
+histColor = ('b', 'g', 'r')
+
+for i in range(3):
+    hist = cv2.calcHist(images=[src], channels=[i], mask=None, histSize=[256], ranges=[0, 256])
+    plt.plot(hist, color=histColor[i])
+
+plt.show()
