bd4f05b319eb3c60ada61405a8982e1c0efcea8d
unexistentman <chan3358@naver.com>
2021-09-02 16:48:16 +0900

    add

---
+++ py/05/0507.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+src = np.array([[0, 0, 0, 0],
+                [1, 1, 3, 5],
+                [6, 1, 1, 3],
+                [4, 3, 1, 7]], dtype=np.uint8)
+
+dst = cv2.equalizeHist(src)
+print('dst =', dst)
